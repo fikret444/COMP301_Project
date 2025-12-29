303704b6d8d4484775e09c2f8bcb43cf42a31383
furk4ny1ld1z <furkan.yildiz2@std.yeditepe.edu.tr>
2025-12-29 16:27:31 +0300

    created general struction for project

---
+++ discovery-server/src/main/java/com/carrental/discovery_server/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.carrental.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer; // Bu import önemli
+
+@SpringBootApplication
+@EnableEurekaServer // <-- BU SATIRI EKLEYİN
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
